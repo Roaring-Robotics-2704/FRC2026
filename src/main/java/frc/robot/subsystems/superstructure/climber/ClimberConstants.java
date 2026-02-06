257cf13ab2206eda019e0f05d276a6d7b401bd18
nisagu <150540975+nisagu@users.noreply.github.com>
2026-02-05 20:49:05 -0600

    climber code

---
+++ src/main/java/frc/robot/subsystems/superstructure/climber/ClimberConstants.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.superstructure.climber;
+
+public class ClimberConstants {
+    // canID
+    public static final int TELESCOPE_MOTOR_ID = 100;
+    public static final int HOOK_MOTOR_ID = 101;
+
+    //
+    public static final double HOOK_TARGET_DEGREES = 180;
+
+    //Climber heights
+    public static final double RETRACTED_HEIGHT = 20.5;
+    public static final double EXTENDED_HEIGHT = 29.5;
+
+    //Speed of climber arm and hook
+    public static final double HOOK_SPEED = .4;
+    public static final double TELESCOPE_SPEED = .6;
+
+    public static final int TELESCOPE_CURRENT_LIMIT = 20;
+    public static final int HOOK_CURRENT_LIMIT = 20;
+}
