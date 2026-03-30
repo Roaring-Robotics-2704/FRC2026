bb8e5bc9ede289e045a9f3bd1e4ad2fd9c8b3fd5
cordelia34 <corh2945@icloud.com>
2026-03-30 17:31:20 -0500

    Update at 'Mon Mar 30 17:31:20 CDT 2026'

--- src/main/java/frc/robot/subsystems/superstructure/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/superstructure/shooter/Shooter.java
@@ -108,7 +108,7 @@ public class Shooter extends SubsystemBase {
                     break;
                  case NonCameraShooting:
                     io.setHoodPercent(0);
-                    io.setFlywheelVelocity(RPM.of(3075));// still need to set the value going to get it after testing;
+                    io.setFlywheelVelocity(RPM.of(3175));// still need to set the value going to get it after testing;
                     break;
                 default:
                     break;
