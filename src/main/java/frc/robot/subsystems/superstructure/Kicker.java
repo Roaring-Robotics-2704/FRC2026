bbdc835883d57ac7a2a735f6aadbc24fcf6d5d3a
nisagu <150540975+nisagu@users.noreply.github.com>
2026-04-11 15:42:24 -0500

    Update at 'Sat Apr 11 15:42:24 CDT 2026'

--- src/main/java/frc/robot/subsystems/superstructure/Kicker.java
+++ src/main/java/frc/robot/subsystems/superstructure/Kicker.java
@@ -58,7 +58,7 @@ public class Kicker extends SubsystemBase {
         //     kickerMotor.setVoltage(-1);
         // } else {
         //     timer.stop();
-        kickerMotor.setVoltage(4);
+        //kickerMotor.setVoltage(4);
         // // }
         // Logger.recordOutput("Kicker/Current", kickerMotor.getOutputCurrent());
         // Logger.recordOutput("Kicker/DesiredVoltage", kickerMotor.getMotorVoltage());
